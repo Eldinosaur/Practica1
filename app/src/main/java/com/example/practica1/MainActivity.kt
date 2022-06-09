036baeb33d3948f1a1ba0cb52e48458718ed7ed9
Eldinosaur <82054019+Eldinosaur@users.noreply.github.com>
2022-06-09 18:51:32 -0500

    jueves 9/06/2022

--- app/src/main/java/com/example/practica1/MainActivity.kt
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.practica1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun imprimirDatos(){
+        var nombres = edt_nombre.text
+        var apellidos = edt_apellido.text
+        btn_confirmar.setOnClickListener{
+            var resultado = nombres +" "+apellidos
+            txt_resultado.setText(resultado)
+        }
     }
 }
