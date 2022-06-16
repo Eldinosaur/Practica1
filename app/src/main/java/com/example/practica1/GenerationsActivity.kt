698fb64b4ada8ce8499b5b7b9ad2d5ef9160821e
Eldinosaur <82054019+Eldinosaur@users.noreply.github.com>
2022-06-16 17:21:26 -0500

    Update GenerationsActivity.kt

--- app/src/main/java/com/example/practica1/GenerationsActivity.kt
+++ app/src/main/java/com/example/practica1/GenerationsActivity.kt
@@ -8,8 +8,25 @@ class GenerationsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generations)
+        imprimirGeneracion()
     }
     fun imprimirGeneracion(){
-        btnVerificar
+        btnVerificar.setOnClickListener{
+            val year = edtNacimiento.text.toString().toInt()
+            when(year){
+                in 1969 .. 1980 ->{
+                    txtGeneracion.text="Generacion X"
+                }
+                in 1981 .. 1993 ->{
+                    txtGeneracion.text="Generacion Y"
+                }
+                in 1994 .. 2010 ->{
+                    txtGeneracion.text="Generacion Z"
+                }
+                else -> {
+                    txtGeneracion.text="No pertenece a ninguna generacion"
+                }
+            }
+        }
     }
 }
