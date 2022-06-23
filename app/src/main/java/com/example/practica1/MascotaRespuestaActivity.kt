268b489e6ab5e5b879aed06740bfcc63145204b3
Eldinosaur <82054019+Eldinosaur@users.noreply.github.com>
2022-06-23 12:43:53 -0500

    activity mascota

---
+++ app/src/main/java/com/example/practica1/MascotaRespuestaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.practica1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MascotaRespuestaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mascota_respuesta)
+    }
+}
